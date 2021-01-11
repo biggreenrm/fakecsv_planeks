0f3080a39ea6be6198a7376971b2073296b4de84
Sergey <biggreen.rm@gmail.com>
2021-01-11 11:39:21 +0300

    create prototype views

--- fake_data/views.py
+++ fake_data/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import (login as auth_login,  authenticate)
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
@@ -20,3 +21,18 @@ def login(request):
             _message = 'Invalid login, please try again.'
     context = {'message': _message}
     return render(request, 'login/login.html', context)
+
+@login_required
+def list_schema(request):
+    context = {}
+    return render(request, 'fake_data/list_schema.html', context)
+
+@login_required
+def new_schema(request):
+    context = {}
+    return render(request, 'fake_data/new_schema.html', context)
+
+@login_required
+def list_dataset(request, id):
+    context = {}
+    return render(request, 'fake_data/list_dataset.html', context)
